88b3653d266fe597e99710d04d48adbf9e2b5fea
Archit Garg <architgarg58@yahoo.in>
2018-12-30 20:16:57 +0530

    Single player enabled

--- app/src/main/java/architgargpro/tictactoe/MainActivity.kt
+++ app/src/main/java/architgargpro/tictactoe/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,7 +33,7 @@ class MainActivity : AppCompatActivity() {
             R.id.button9 -> cellID = 9
         }
 
-        Toast.makeText(this, "Button Clicked : " + cellID , Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Button Clicked : " + cellID , Toast.LENGTH_SHORT).show()
 
         play(cellID, selectedButton)
     }
@@ -40,24 +42,125 @@ class MainActivity : AppCompatActivity() {
     var player2 = ArrayList<Int>()
 
     var activePlayer = 1
+    var winner = 0 //1 for win 0 for no winner
 
     private fun play(cellID: Int, selectedButton: Button)
     {
         if(activePlayer == 1)
         {
             selectedButton.text = "O"
-            selectedButton.setTextColor(Color.parseColor("#00574B"))
+            selectedButton.setTextColor(Color.parseColor("#ffffff"))
             player1.add(cellID)
             activePlayer = 2
+
+            checkWin()
+
+
+
+            // play the automated turn after a small pause
+            if(winner==0) {
+
+                autoPlay()
+            }
         }
         else
         {
             selectedButton.text = "X"
-            selectedButton.setTextColor(Color.parseColor("#00574B"))
+            selectedButton.setTextColor(Color.parseColor("#ffffff"))
             player2.add(cellID)
             activePlayer = 1
         }
 
         selectedButton.isEnabled = false
+        checkWin()
+    }
+
+    private fun checkWin()
+    {
+        if((player1.contains(1) && player1.contains(2) && player1.contains(3)) ||
+            (player1.contains(4) && player1.contains(5) && player1.contains(6)) ||
+            (player1.contains(7) && player1.contains(8) && player1.contains(9)) ||
+            (player1.contains(1) && player1.contains(4) && player1.contains(7)) ||
+            (player1.contains(2) && player1.contains(5) && player1.contains(8)) ||
+            (player1.contains(3) && player1.contains(6) && player1.contains(9)) ||
+            (player1.contains(1) && player1.contains(5) && player1.contains(9)) ||
+            (player1.contains(3) && player1.contains(5) && player1.contains(7)))
+        {
+            winner = 1
+        }
+        else
+            if((player2.contains(1) && player2.contains(2) && player2.contains(3)) ||
+                    (player2.contains(4) && player2.contains(5) && player2.contains(6)) ||
+                    (player2.contains(7) && player2.contains(8) && player2.contains(9)) ||
+                    (player2.contains(1) && player2.contains(4) && player2.contains(7)) ||
+                    (player2.contains(2) && player2.contains(5) && player2.contains(8)) ||
+                    (player2.contains(3) && player2.contains(6) && player2.contains(9)) ||
+                    (player2.contains(1) && player2.contains(5) && player2.contains(9)) ||
+                    (player2.contains(3) && player2.contains(5) && player2.contains(7)))
+        {
+            winner = 2
+        }
+
+        if(winner!=0) {
+            Toast.makeText(this, "Player " + winner.toString() + " Won", Toast.LENGTH_LONG).show()
+
+
+            //disabling other buttons also to end the game
+            for (i in 1..9)
+            {
+                if(!(player1.contains(i) || player2.contains(i)))
+                {
+                    val selectedButton: Button
+                    when (i) {
+                        1 -> selectedButton = button1
+                        2 -> selectedButton = button2
+                        3 -> selectedButton = button3
+                        4 -> selectedButton = button4
+                        5 -> selectedButton = button5
+                        6 -> selectedButton = button6
+                        7 -> selectedButton = button7
+                        8 -> selectedButton = button8
+                        9 -> selectedButton = button9
+                        else -> selectedButton = button1
+                    }
+                    selectedButton.isEnabled = false
+                }
+            }
+
+        }
+    }
+
+    private fun autoPlay()
+    {
+        val emptyCells = ArrayList<Int>()
+        for (i in 1..9)
+        {
+            if(!(player1.contains(i) || player2.contains(i)))
+            {
+                emptyCells.add(i)
+            }
+        }
+
+        if(emptyCells.size!=0) {
+
+            val randomIndex = (Math.random() * emptyCells.size).toInt()
+            val cellID = emptyCells[randomIndex]
+
+            val selectedButton: Button
+            when (cellID) {
+                1 -> selectedButton = button1
+                2 -> selectedButton = button2
+                3 -> selectedButton = button3
+                4 -> selectedButton = button4
+                5 -> selectedButton = button5
+                6 -> selectedButton = button6
+                7 -> selectedButton = button7
+                8 -> selectedButton = button8
+                9 -> selectedButton = button9
+                else -> selectedButton = button1
+            }
+
+            play(cellID, selectedButton)
+        }
     }
 }
